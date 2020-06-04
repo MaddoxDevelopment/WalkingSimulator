f4c98ec7a0d3a064ef7a85572ea6b83db127d780
Jon Maddox <jm@madev.me>
2020-06-04 18:41:21 -0500

    opengl & glfw

---
+++ WalkingSimulator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using GLFW;
+using OpenGL;
+
+namespace WalkingSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gl.Initialize();
+            using (var window = new NativeWindow(800, 600, "Walking Simulator"))
+            {
+                Glfw.MakeContextCurrent(window);
+                Gl.BindAPI();
+   
+                while (!window.IsClosing)
+                {
+                    Gl.ClearColor(0.0F, 0.0F, 0.0F, 1.0F);
+                    Gl.Clear((ClearBufferMask) Gl.COLOR_BUFFER_BIT);
+                    Console.WriteLine(Glfw.Time);
+                    window.SwapBuffers();
+                    Glfw.PollEvents();
+                }
+            }
+        }
+    }
+}
